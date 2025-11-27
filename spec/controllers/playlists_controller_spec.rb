c261317dcc94f2a6656b60120b145ad98cac8793
vanessalobo <vanessa.lobo0211@gmail.com>
2025-11-27 10:25:30 -0600

    Add rspec tests for create playlist
    Added rspec tests for create playlist

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -109,4 +109,79 @@ RSpec.describe PlaylistsController, type: :controller do
             end
         end
     end
+
+    describe "POST #create_from_recommendations" do
+        let(:uris) { ["spotify:track:1", "spotify:track:2"] }
+
+        context "when user is not logged in" do
+            before { session.delete(:spotify_user) }
+
+            it "redirects to root with alert" do
+                post :create_from_recommendations, params: { uris: uris }
+                expect(response).to redirect_to(root_path)
+                expect(flash[:alert]).to eq("Please sign in with Spotify first.")
+            end
+        end
+
+        context "when no uris provided" do
+            it "redirects back with no tracks alert" do
+                post :create_from_recommendations, params: { uris: [] }
+                expect(response).to redirect_to(recommendations_path)
+                expect(flash[:alert]).to eq("No tracks to add to playlist.")
+            end
+        end
+
+        context "when spotify creates playlist successfully" do
+            before do
+                allow(spotify_client).to receive(:current_user_id).and_return("user123")
+                allow(spotify_client).to receive(:create_playlist_for).and_return("playlist_abc")
+                allow(spotify_client).to receive(:add_tracks_to_playlist).and_return(true)
+            end
+
+            it "creates playlist and redirects with notice" do
+                post :create_from_recommendations, params: { uris: uris, playlist_name: "My Recs" }
+
+                expect(spotify_client).to have_received(:create_playlist_for).with(
+                    user_id: "user123",
+                    name: "My Recs",
+                    description: "Auto-created from Spotilytics • Your Recommendations",
+                    public: false
+                )
+
+                expect(spotify_client).to have_received(:add_tracks_to_playlist).with(
+                    playlist_id: "playlist_abc",
+                    uris: uris
+                )
+
+                expect(response).to redirect_to(recommendations_path)
+                expect(flash[:notice]).to eq("Playlist created on Spotify: My Recs")
+            end
+        end
+
+        context "when spotify raises unauthorized" do
+            before do
+                allow(spotify_client).to receive(:current_user_id).and_return("user123")
+                allow(spotify_client).to receive(:create_playlist_for).and_raise(SpotifyClient::UnauthorizedError)
+            end
+
+            it "redirects to root with alert" do
+                post :create_from_recommendations, params: { uris: uris }
+                expect(response).to redirect_to(root_path)
+                expect(flash[:alert]).to eq("Session expired. Please sign in with Spotify again.")
+            end
+        end
+
+        context "when spotify raises generic error" do
+            before do
+                allow(spotify_client).to receive(:current_user_id).and_return("user123")
+                allow(spotify_client).to receive(:create_playlist_for).and_raise(SpotifyClient::Error.new("boom"))
+            end
+
+            it "redirects back with error message" do
+                post :create_from_recommendations, params: { uris: uris }
+                expect(response).to redirect_to(recommendations_path)
+                expect(flash[:alert]).to eq("Couldn't create playlist on Spotify: boom")
+            end
+        end
+    end
 end
